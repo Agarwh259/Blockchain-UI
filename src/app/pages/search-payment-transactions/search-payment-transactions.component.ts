9fa5a1671bf64e1a303b4aed8880ea7ad33c41e3
Akshay3253 <45037016+Akshay3253@users.noreply.github.com>
2018-11-30 17:34:15 +0530

    Search Payment screen service integration

--- src/app/pages/search-payment-transactions/search-payment-transactions.component.ts
+++ src/app/pages/search-payment-transactions/search-payment-transactions.component.ts
@@ -5,6 +5,11 @@ import { PaymentSearchTransaction } from 'src/app/shared/models/transaction/paym
 import { Property } from '../property';
 import { IssuerType } from 'src/app/shared/models/transaction/issuer-type.enum';
 import TransactionType from 'src/app/shared/models/transaction/transaction-type.enum';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { TransactionService } from 'src/app/shared/services/transaction/transaction.service';
+import { LoggerService } from 'src/app/shared/services/logging/logger.service';
+import Loglevel from 'src/app/shared/models/logging/loglevel.enum';
+import { NotificationsService, NotificationType } from 'angular2-notifications';
 
 @Component({
   selector: 'app-search-payment-transactions',
@@ -25,7 +30,9 @@ export class SearchPaymentTransactionsComponent implements OnInit {
 
   searchPaymentEntity : PaymentSearchTransaction;
 
-  constructor(private data: DataService, private modalService: NgbModal) { }
+  constructor(private data: DataService, private modalService: NgbModal,
+    private spinner: NgxSpinnerService, private transactionService: TransactionService,
+    private logger: LoggerService, private _service: NotificationsService) { }
 
   ngOnInit() {
     this.userName = sessionStorage.getItem('Role').toLowerCase();
@@ -52,21 +59,12 @@ export class SearchPaymentTransactionsComponent implements OnInit {
     ];
   }
 
-  // GetInvoiceTransaction() {
-  //   this.showSearchResults = true;
-  //   this.rawSearchResults = this.data.getSearchResult();
-
-  //   this.paymentFilteredRecords = this.rawSearchResults.filter(function (el) {
-  //     return el.TransactionType == "Payment"
-  //   });
-  // }
-
   openModal(content, ClickedTransactionId) {
     this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
     }, (reason) => {
     });
 
-    this.modalTransactionDetails = this.rawSearchResults.filter(function (el) {
+    this.modalTransactionDetails = this.paymentFilteredRecords.filter(function (el) {
       return el.transactionId === ClickedTransactionId
     });
 
@@ -88,14 +86,68 @@ export class SearchPaymentTransactionsComponent implements OnInit {
 
   getSearchResults(entity : PaymentSearchTransaction)
   {
+    this.spinner.show();
     console.log(entity.caseNumber);
     this.showSearchResults = true;
-    this.rawSearchResults = this.data.getInvoiceAndPaymentData().filter(function(el){
-      return el.transactionType === TransactionType.Payment
-    })
-    
-    this.paymentFilteredRecords = this.rawSearchResults.filter(function(el){
-      return el.caseNumber == entity.caseNumber;
-    });
+
+    this.paymentFilteredRecords = [];
+
+    this.transactionService.searchPayment(+entity.caseNumber , 'MCO')
+    .then(
+      (res) => { this.logger.Log(res, Loglevel.Warning);
+        this.paymentFilteredRecords.push(
+          {
+            transactionId : res.transactionId,
+            transactionType : res.transactionType,
+            caseNumber : res.caseNumber,
+            coverageMonth: res.coverageMonth,
+            issuerId: res.issuerId,
+            invoiceDate: res.invoiceDate,
+            dueDate: res.dueDate,
+            premiumAmount: res.premiumAmount,
+            paymentStatus: res.paymentStatus,
+            paymentDate : res.paymentDate,
+            processedByIEES: res.processedByIEES,
+            processedByMCO: res.processedByMCO
+          }         
+        );
+        
+        this.spinner.hide();
+        }
+    )
+    .catch(
+      (res) => { 
+        this.logger.Log(res, Loglevel.Error); 
+        this.spinner.hide();
+      }
+    )
   }
+
+  SubmitDetails(item)
+{
+  console.log(item);
+  console.log(this.userName);
+  if(this.userName === 'iees' && item.processedByIEES === 'Y')
+  {
+    if(item.transactionType === 'Invoice')
+    {
+      console.log(item);
+      this.transactionService.updatePayment(item.caseNumber, 'IEES')
+      .then(result =>
+        {
+          console.log(result);
+          console.log(result.status);
+         // alert("submission successful");
+         this._service.create('Success','"Invoice processed successfully"',NotificationType.Success);
+       
+        })
+      .catch(error => {
+        console.log(error);
+        console.log(error.status);
+        this._service.create('Error','"Something went wrong. Unable to submit transaction.!"',NotificationType.Error);
+     
+      });
+    }
+  }
+}
 }
--- src/app/pages/search-payment-transactions/search-payment-transactions.module.ts
+++ src/app/pages/search-payment-transactions/search-payment-transactions.module.ts
@@ -4,7 +4,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { routing } from './search-payment-transactions.routing';
 import { SharedModule } from '../../shared/shared.module';
 import { SearchPaymentTransactionsComponent } from './search-payment-transactions.component';
-
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { SimpleNotificationsModule } from 'angular2-notifications';
 
 @NgModule({
     imports: [
@@ -12,7 +13,9 @@ import { SearchPaymentTransactionsComponent } from './search-payment-transaction
         FormsModule,
         ReactiveFormsModule,
         SharedModule,
-        routing
+        routing,
+        NgxSpinnerModule,
+        SimpleNotificationsModule.forRoot()
     ],
     declarations: [
         SearchPaymentTransactionsComponent    ]
